5d53c407327ee1f2452e4ab22b5bba6412860968
Гамид Халидов <khalidov-gamid@mail.ru>
2024-06-23 18:30:13 +0300

    Initial Commit

---
+++ Pic&Name/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pic&Name
+//
+//  Created by Gamıd Khalıdov on 23.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pic&Name/Pic_NameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pic_NameApp.swift
+//  Pic&Name
+//
+//  Created by Gamıd Khalıdov on 23.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Pic_NameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
